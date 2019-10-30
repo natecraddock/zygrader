873c54e182e99650b4ee76748a023c47fd46a16c
Nathan Craddock <nzcraddock@gmail.com>
2019-10-29 19:48:29 -0600

    Lock: Handle terminal exiting unexpectedly
    Clear all lock files associated with a user if the terminal exits
    unexpectedly. This is to ensure that the person doesn't stay locked and
    others cannot grade it.

--- __main__.py
+++ __main__.py
@@ -1,3 +1,16 @@
+import getpass
+import signal
+import sys
+
 from zygrader import zygrade
+from zygrader.data import lock
+
+def lock_cleanup(signum, frame):
+    lock.unlock_all_labs_by_grader(getpass.getuser())
+    sys.exit(0)
+
+# Handle Signals
+signal.signal(signal.SIGTERM, lock_cleanup)
+signal.signal(signal.SIGHUP, lock_cleanup)
 
 zygrade.start()
