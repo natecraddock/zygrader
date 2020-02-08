9e12b760ec269c00a0ef42f102615f67555f853c
Nathan Craddock <nzcraddock@gmail.com>
2020-02-07 20:06:30 -0700

    Allow running zygrader from any directory

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import curses
 import getpass
 import signal
@@ -35,6 +36,11 @@ def sigtstp_handler(signum, frame):
 signal.signal(signal.SIGINT, sigint_handler)
 signal.signal(signal.SIGTSTP, sigtstp_handler)
 # signal.signal(signal.SIGHUP, lock_cleanup)
+
+# Ensure zygrader starts in the proper directory (one level up from the __main__.py file)
+zygrader_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(zygrader_path)
+os.chdir("..")
 
 zygrader.start()
 
