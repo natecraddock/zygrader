c852f61294959100e9f441b385060c781c533866
Nathan Craddock <nzcraddock@gmail.com>
2020-07-27 11:58:45 -0600

    Versioning: Bump to 4.2.0

--- zygrader/config/shared.py
+++ zygrader/config/shared.py
@@ -7,7 +7,7 @@ from . import preferences
 
 class SharedData:
     # Zygrader version
-    VERSION = LooseVersion("4.1.2")
+    VERSION = LooseVersion("4.2.0")
 
     # Current class code (shared)
     # Can be overridden on a user level
--- zygrader/config/versioning.py
+++ zygrader/config/versioning.py
@@ -69,5 +69,11 @@ def do_versioning(window: Window):
 
         window.create_popup(f"Version {version}", msg, components.Popup.ALIGN_LEFT)
 
+    version = "4.2.0"
+    if compare_versions(version, user_version):
+        msg = get_version_message(version)
+
+        window.create_popup(f"Version {version}", msg, components.Popup.ALIGN_LEFT)
+
     # Write the current version to the user's config file
     write_current_version(config)
