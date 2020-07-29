58067b89f9d7ab23c03f4b2d6893a0aa084ce414
cs142ta <cs142question@gmail.com>
2020-07-29 12:05:04 -0600

    Bump version

--- zygrader/config/shared.py
+++ zygrader/config/shared.py
@@ -7,7 +7,7 @@ from . import preferences
 
 class SharedData:
     # Zygrader version
-    VERSION = LooseVersion("4.3.2")
+    VERSION = LooseVersion("4.3.3")
 
     # Current class code (shared)
     # Can be overridden on a user level
