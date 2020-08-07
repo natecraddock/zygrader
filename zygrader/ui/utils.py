bd48436a5f7b01317dd42b4e51481bcdbc6e652e
cs142ta <cs142question@gmail.com>
2020-08-07 13:40:58 -0600

    Add wrapper for addch like wrapper for addstr

--- zygrader/ui/utils.py
+++ zygrader/ui/utils.py
@@ -1,19 +1,29 @@
 import curses
 from zygrader.ui.displaystring import DisplayStr
 
-def add_str(window, y, x, text, attrs=0):
-    """Wrapper around addstr to catch errors"""
-    try:
-        if isinstance(text, DisplayStr):
-            xslide = x
-            for segment in text.segments:
-                text, extra_attrs = segment
-                window.addstr(y, xslide, text, extra_attrs | attrs)
-                xslide += len(text)
-        else:
-            window.addstr(y, x, text, attrs)
-    except curses.error:
-        pass
+def text_adder(func):
+    """Wrappers around addstr/addch to catch errors"""
+    def wrapped(window, y, x, text, attrs=0):
+        try:
+            if isinstance(text, DisplayStr):
+                xslide = x
+                for segment in text.segments:
+                    text, extra_attrs = segment
+                    func(window, y, xslide, text, extra_attrs | attrs)
+                    xslide += len(text)
+            else:
+                func(window, y, x, text, attrs)
+        except curses.error:
+            pass
+    return wrapped
+
+@text_adder
+def add_str(window, y, x, text, attrs):
+    window.addstr(y, x, text, attrs)
+
+@text_adder
+def add_ch(window, y, x, text, attrs):
+    window.addch(y, x, text, attrs)
 
 def resize_window(window, rows, cols):
     """Wrapper around resize to catch errors"""
