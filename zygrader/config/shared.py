264a19cbb936204a3e34e65a236ed539c727869f
Nathan Craddock <nzcraddock@gmail.com>
2020-09-17 15:26:11 -0600

    Bump version to 4.7.0
    Email management is a bigger feature, so do a subversion.
    
    5.0.0 will be the window manager refactor :)

--- zygrader/config/shared.py
+++ zygrader/config/shared.py
@@ -7,7 +7,7 @@ from . import preferences
 
 class SharedData:
     # Zygrader version
-    VERSION = LooseVersion("4.6.8")
+    VERSION = LooseVersion("4.7.0")
 
     # Current class code (shared)
     # Can be overridden on a user level
