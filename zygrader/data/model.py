natecraddock/zygrader

commit bd7ccba85ccf80606585948dcd14a7d08124c9d9

Fix #5: Make key input blocking to reduce CPU usage When input was non-blocking, the input read loop continuously executed, ramping up CPU usage and essentially taking over a core
Now, the execution waits until input is actually present

zygrader/ui/window.py
@@ -101,7 +101,6 @@ class Window:
         event_value = Event.NONE
         event_mod = None
 
-        # Nodelay causes exception when no input is given
         input_code = input_win.getch()
         if input_code == -1:
             return Event(event, event_value)
@@ -211,7 +210,8 @@ class Window:
         # Create window for input
         input_win = curses.newwin(0, 0, 1, 1)
         input_win.keypad(True)
-        input_win.nodelay(True)
+        # Makes getch blocking to reduce CPU usage
+        input_win.nodelay(False)
 
         while True:
             self.take_input.wait()

commit 233ea80dc436998cb4d9ef4b5ecc181593a0d5dc

Fix: Input errors after switching to delayed input
Regression caused by bd7ccba85ccf80606585948dcd14a7d08124c9d9

The simple solution is to just remove alt-based input because it is not widely
supported over SSH.

zygrader/data/model.py
@@ -392,8 +392,8 @@ class Submission(Iterable):
         print(f"Running {self.student.full_name}'s code")
         print("CTRL+C to terminate")
         print("CTRL+Z to stop (pause)")
-        if not use_gdb:
-            print("ALT+ENTER when running code to use gdb")
+        # if not use_gdb:
+        # print("ALT+ENTER when running code to use gdb")
         print("#############################################################\n")
 
         if use_gdb:

zygrader/grader.py
@@ -127,7 +127,7 @@ def view_diff(first: model.Submission, second: model.Submission):
 
 def run_code_fn(window, context: ui.WinContext, submission):
     """Callback to compile and run a submission's code"""
-    use_gdb = context.event.modifier == ui.Event.MOD_ALT
+    use_gdb = False
 
     if not submission.compile_and_run_code(use_gdb):
         window.create_popup("Error", ["Could not compile and run code"])

zygrader/ui/window.py
@@ -38,9 +38,6 @@ class Event:
     SHOME = 18
     SEND = 19
 
-    # Modifier Keys
-    MOD_ALT = 0
-
     def __init__(self, event_type, value, modifier=None):
         self.type = event_type
         self.value = value
@@ -104,22 +101,6 @@ class Window:
         input_code = input_win.getch()
         if input_code == -1:
             return Event(event, event_value)
-
-        # Check for ALT pressed
-        elif input_code == 27:
-            # Check for next character
-            input_code = input_win.getch()
-            if input_code == -1:
-                # Interpret as a single ESC char
-                if self.insert_mode:
-                    event = self.set_mode(Window.MODE_NORMAL)
-                elif self.mark_mode:
-                    event = self.set_mode(Window.MODE_NORMAL)
-                else:
-                    event = Event.ESC
-                return Event(event, event_value)
-            # Character accepted, check next symbol
-            event_mod = Event.MOD_ALT
 
         # Cases for each type of input
         if input_code == curses.KEY_RESIZE: