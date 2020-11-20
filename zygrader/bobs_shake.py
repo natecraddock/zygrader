9e9eb80a4bec5a5ed531c8420eeec2105706253b
Grace Kenny <graceksemail@gmail.com>
2020-11-20 12:59:23 -0700

    fixed style

--- zygrader/bobs_shake.py
+++ zygrader/bobs_shake.py
@@ -5,11 +5,12 @@ import csv
 
 
 def start():
+    #call get get_lock_log_path()
     pass
 
 
-class grading_stats_worker:
-    class work_event:
+class GradingStatsWorker:
+    class WorkEvent:
         def __init__(self, row):
             time_stamp = row[0]
             event_type = row[1]
@@ -21,7 +22,7 @@ class grading_stats_worker:
         def is_lock(self):
             return self.lock_type == "LOCK"
 
-    class student_assignment:
+    class StudentAssignment:
         def __init__(self, student, lab):
             student_name = student
             lab_name = lab
@@ -36,9 +37,9 @@ class grading_stats_worker:
         with open(file_name, 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
             for row in csv_reader:
-                event = grading_stats_worker.work_event(row)
+                event = GradingStatsWorker.work_event(row)
                 if (event.event_type == "LAB"):
-                    grading_stats_worker.events.append(event)
+                    GradingStatsWorker.events.append(event)
 
     events = []
     mappedEvents = {}
