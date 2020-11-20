7805b8589a86c173135b8f7069739d6ed0e68e36
Grace Kenny <graceksemail@gmail.com>
2020-11-19 22:12:27 -0700

    Worked on grading stats worker class
    Finished up work_events constructor and started assignments

--- zygrader/bobs_shake.py
+++ zygrader/bobs_shake.py
@@ -2,19 +2,43 @@
 #make an event class
 
 import csv
+
+
 def start():
     pass
-class work_event:
-    def __init__(self, row):
-        pass
-        #I haven't figured out how to store the row's fields into variablesrow_field = next(row)
 
-def read_in_stats(file_name):
-    """takes in a csv file and goes through it, using each row to make an event"""
-    with open(file_name, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        for row in csv_reader:
-            event = work_event(row)
-            events.append(event) #if event type is LAB:
-events = []
-mappedEvents = {}
+
+class grading_stats_worker:
+    class work_event:
+        def __init__(self, row):
+            time_stamp = row[0]
+            event_type = row[1]
+            student_name = row[2]
+            lab_name = row[3]
+            ta_netid = row[4]
+            lock_type = row[5]
+
+        def is_lock(self):
+            return self.lock_type == "LOCK"
+
+    class student_assignment:
+        def __init__(self, student, lab):
+            student_name = student
+            lab_name = lab
+
+        def __lt__(self, other_assignment):
+            if (self.student_name == other_assignment.student_name):
+                return self.lab_name < other_assignment.lab_name
+            return self.student_name < other_assignment.student_name
+
+    def read_in_stats(file_name):
+        """takes in a csv file and goes through it, using each row to make an event"""
+        with open(file_name, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                event = grading_stats_worker.work_event(row)
+                if (event.event_type == "LAB"):
+                    grading_stats_worker.events.append(event)
+
+    events = []
+    mappedEvents = {}
