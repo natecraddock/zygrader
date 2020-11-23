5580b7d690a4d5cb9a07684edb471cb98918ab3e
Nathan Craddock <nzcraddock@gmail.com>
2020-11-23 08:40:48 -0700

    Fix: Wrong version number in code

--- zygrader/config/shared.py
+++ zygrader/config/shared.py
@@ -8,7 +8,7 @@ from . import preferences
 
 class SharedData:
     # Zygrader version
-    VERSION = LooseVersion("5.2.0")
+    VERSION = LooseVersion("5.2.2")
 
     # Current class code (shared)
     # Can be overridden on a user level
