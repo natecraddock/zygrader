7ab1ed6d2d0b5f1c92e31cee705b4ddef0f5de63
Grace Kenny <graceksemail@gmail.com>
2020-11-24 17:57:41 -0700

    Added to debugging functionality
    Made a debugging function to write out to a file and left some comments on stuff that is broken

--- zygrader/bobs_shake.py
+++ zygrader/bobs_shake.py
@@ -1,5 +1,6 @@
 #TODO: everything
 #make an event class
+#WorkEvent init doesn't work and gives all kinds of fun errors
 
 import csv
 from zygrader.data.lock import get_lock_log_path
@@ -9,13 +10,21 @@ def start():
     #call get get_lock_log_path()
     test = GradingStatsWorker()
     log_file = get_lock_log_path()
-    #test.read_in_stats(log_file)
-    pass
+    test.read_in_stats(log_file)
 
 
 class GradingStatsWorker:
+    def debug_grading(self, print_message):
+        """This is for debugging purposes. It will write out whatever you pass to it
+        to a file called bobsOutput.txt which is saved in your home directory"""
+        debug_output = open("bobsOutput.txt", 'w')
+        print(print_message, file=debug_output)
+        debug_output.close()
+
     class WorkEvent:
         def __init__(self, row):
+            """This isn't working for some reason. 
+            Maybe has to do with accessing out of range -GK"""
             time_stamp = row[0]
             event_type = row[1]
             student_name = row[2]
@@ -36,12 +45,15 @@ class GradingStatsWorker:
                 return self.lab_name < other_assignment.lab_name
             return self.student_name < other_assignment.student_name
 
-    def read_in_stats(file_name):
+    def read_in_stats(self, file_name):
         """takes in a csv file and goes through it, using each row to make an event"""
         with open(file_name, 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
             for row in csv_reader:
-                event = GradingStatsWorker.work_event(row)
+
+                self.debug_grading(row)
+
+                event = GradingStatsWorker.WorkEvent(row)
                 if (event.event_type == "LAB"):
                     GradingStatsWorker.events.append(event)
 
