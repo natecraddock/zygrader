fd36a56faed6974ee3f886181bfdd886e05d374c
Teikn Smith <spencerteiknsmith@gmail.com>
2021-01-28 20:48:20 -0700

    Move classes to top level of module

--- zygrader/bobs_shake.py
+++ zygrader/bobs_shake.py
@@ -6,6 +6,7 @@ from collections import namedtuple
 import csv
 import datetime
 import time
+import typing
 
 from zygrader import ui
 from zygrader.data.lock import get_lock_log_path
@@ -23,7 +24,9 @@ def start():
         Step(True, "end time", lambda: worker.select_end_time()),
         Step(False, "Read data from the log file",
              lambda: worker.read_in_native_stats()),
-        Step(True, "Debug show events", lambda: worker.show_events())
+        Step(True, "Debug show events", lambda: worker.show_events()),
+        Step(False, "Assign events to individual tas",
+             lambda: worker.assign_events_to_tas())
     ]
 
     for step in steps:
@@ -38,6 +41,65 @@ def start():
                 return
 
 
+class WorkEvent:
+    def __init__(self, row):
+        time_stamp_str = row[0]
+        # TODO: convert to datetime.datetime.fromisoformat
+        #   once we are using python 3.7
+        self.time_stamp = datetime.datetime.strptime(time_stamp_str,
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+
+        # The old lock format did not have an event_type field
+        # (all locks were for labs)
+        is_old_format = len(row) == 5
+        if is_old_format:
+            self.event_type = 'LAB'
+        else:
+            self.event_type = row[1]
+
+        old_format_shift = 1 if is_old_format else 0
+        self.student_name = row[2 - old_format_shift]
+        self.lab_name = row[3 - old_format_shift]
+        self.ta_netid = row[4 - old_format_shift]
+
+        lock_type = row[5 - old_format_shift]
+        self.is_lock = lock_type == "LOCK"
+
+    def __str__(self):
+        return (f"At {self.time_stamp} {self.ta_netid} "
+                f"{'' if self.is_lock else 'un'}locked "
+                f"{self.student_name}'s {self.event_type}"
+                f"{f' {self.lab_name}' if self.event_type == 'LAB' else ''}")
+
+
+class TA:
+    def __init__(self, netid):
+        self.netid = netid
+        self.events = []
+
+    def add_event(self, event):
+        self.events.append(event)
+
+
+class StudentAssignment:
+    def __init__(self, student, lab):
+        student_name = student
+        lab_name = lab
+
+    def __lt__(self, other_assignment):
+        if (self.student_name == other_assignment.student_name):
+            return self.lab_name < other_assignment.lab_name
+        return self.student_name < other_assignment.student_name
+
+
+def select_time(title: str):
+    time_selector = ui.layers.DatetimeSpinner(title)
+    time_selector.set_quickpicks([(59, 59), (0, 0)])
+    ui.get_window().run_layer(time_selector, "Bob's Shake")
+
+    return (None if time_selector.was_canceled() else time_selector.get_time())
+
+
 class StatsWorker:
     def __init__(self):
         """This is supposed to clear the bob's output before
@@ -47,21 +109,14 @@ class StatsWorker:
         debug_output.close()
 
         self.events = []
-
-    def select_time(self, title: str):
-        time_selector = ui.layers.DatetimeSpinner(title)
-        time_selector.set_quickpicks([(59, 59), (0, 0)])
-        ui.get_window().run_layer(time_selector, "Bob's Shake")
-
-        return (None
-                if time_selector.was_canceled() else time_selector.get_time())
+        self.tas = dict()
 
     def select_start_time(self):
-        self.start_time = self.select_time("Start Time")
+        self.start_time = select_time("Start Time")
         return self.start_time
 
     def select_end_time(self):
-        self.end_time = self.select_time("End Time")
+        self.end_time = select_time("End Time")
         return self.end_time
 
     def debug_grading(self, print_message):
@@ -71,47 +126,6 @@ class StatsWorker:
         debug_output = open("bobsOutput.txt", 'a')
         print(print_message, file=debug_output)
         debug_output.close()
-
-    class WorkEvent:
-        def __init__(self, row):
-            time_stamp_str = row[0]
-            # TODO: convert to datetime.datetime.fromisoformat
-            # once we are using python 3.7
-            self.time_stamp = datetime.datetime.strptime(
-                time_stamp_str, "%Y-%m-%dT%H:%M:%S.%f")
-
-            # The old lock format did not have an event_type field
-            # (all locks were for labs)
-            is_old_format = len(row) == 5
-            if is_old_format:
-                self.event_type = 'LAB'
-            else:
-                self.event_type = row[1]
-
-            old_format_shift = 1 if is_old_format else 0
-            self.student_name = row[2 - old_format_shift]
-            self.lab_name = row[3 - old_format_shift]
-            self.ta_netid = row[4 - old_format_shift]
-
-            lock_type = row[5 - old_format_shift]
-            self.is_lock = lock_type == "LOCK"
-
-        def __str__(self):
-            return (
-                f"At {self.time_stamp} {self.ta_netid} "
-                f"{'' if self.is_lock else 'un'}locked "
-                f"{self.student_name}'s {self.event_type}"
-                f"{f' {self.lab_name}' if self.event_type == 'LAB' else ''}")
-
-    class StudentAssignment:
-        def __init__(self, student, lab):
-            student_name = student
-            lab_name = lab
-
-        def __lt__(self, other_assignment):
-            if (self.student_name == other_assignment.student_name):
-                return self.lab_name < other_assignment.lab_name
-            return self.student_name < other_assignment.student_name
 
     def read_in_native_stats(self):
         """goes through zygrader's lock log, using each row to make an event"""
@@ -124,6 +138,15 @@ class StatsWorker:
                 if (event.time_stamp > self.start_time
                         and event.time_stamp < self.end_time):
                     self.events.append(event)
+        # FIXME: Remove this arbitrary sleep (used for debugging)
+        time.sleep(1)
+
+    def assign_events_to_tas(self):
+        for event in self.events:
+            if event.ta_netid not in self.tas:
+                self.tas[event.ta_netid] = StatsWorker.TA(event.ta_netid)
+            self.tas[event.ta_netid].add_event(event)
+
         # FIXME: Remove this arbitrary sleep (used for debugging)
         time.sleep(1)
 
