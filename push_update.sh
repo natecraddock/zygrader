03cbec518583f8ac7742571cfbdedf0e3de42b3f
Nathan Craddock <nzcraddock@gmail.com>
2021-04-10 09:09:09 -0600

    Add error checking to push_update.sh
    Ensure that shared.py includes the correct version.
    Also require that the latest commit raised the version.

--- push_update.sh
+++ push_update.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+# Requires the version number as an arg
 if [ $# != 1 ]
 then
     echo "Usage: $0 VERSION_NUMBER"
     exit
 fi
 
+# Ensure we are pushing the correct version
+if ! grep -q $1 "./zygrader/config/shared.py";
+then
+    echo "$1 is not the current version in ./zygrader/config/shared.py!"
+    exit
+fi
+
+# Check if most recent commit includes a raise of version number
+# to this current version.
+if ! git show HEAD | grep -q $1;
+then
+    echo "The most recent commit did not bump version to $1"
+    exit
+fi
+
+# Tag and push
 git tag -m "Version $1" $1
 git push --follow-tags
